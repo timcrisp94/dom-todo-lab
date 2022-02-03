e7711a4e8fcee146a0bb5c29e23e33f9a3e7d685
Tim Crisp <timothycrisp@gmail.com>
2022-02-02 18:46:43 -0600

    finish lab proper

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("hi");
+const input = document.querySelector('input')
+
+
+const btn = document.querySelector("#submit-button")
+btn.addEventListener('click', function(evt) {
+  const newLi = document.createElement('li')
+  const inp = document.querySelector('input')
+  newLi.textContent = inp.value
+  if (inp.value !== '') {
+    document.querySelector('ul').appendChild(newLi)
+    inp.value = ''
+    console.log(newLi)
+  }
+
+});
+
+
+const uList = document.querySelector('#todo-list')
+
